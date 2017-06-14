c5db7ad0d03007eeab60e2c6fdbcb7b16c77cbdb
Lazaro Pereira <spmlaz@gmail.com>
2017-06-14 19:36:57 -0400

    Updated with new Background Updated the device status dictionary with one that is more sane.
    Added Return to home links for all result types.

--- app/views.py
+++ app/views.py
@@ -8,9 +8,9 @@ import credentials
 
 @application.route('/')
 @application.route('/index')
-
 def index():
     return render_template('index.html', title='Device Status')
+
 
 
 @application.route("/results", methods=['POST'])
@@ -20,13 +20,14 @@ def results():
     payload = {"j_username": credentials.username, "j_password": credentials.password, 'submit': 'Log In'}
     r = session.post(url='https://'+ credentials.server + '/j_security_check', data=payload, verify=False)
     url = session.get(url='https://' + credentials.server + '/dataservice/system/device/vedges/', verify=False)
-    print(r.url)
-    #serial = '11OD2143152882'
     serial = request.form['serial']
     for i in json.loads(url.text)['data']:
         # pprint.pprint(json.loads(data.text)['data'][i])
         if serial in i.values():
-            responsedict = {'Device': serial, 'Status': i['deviceState'], 'operatingmode': i['configOperationMode']}
+            responsedict = {'Device': serial,
+                            'Status': i['configStatusMessage'],
+                            'statusdetail': i['configStatusMessageDetails'],
+                            'operatingmode': i['configOperationMode']}
         try:
             responsedict
         except NameError:
