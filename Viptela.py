b3044db320c49e429a2f5d8903b1321193c9bd04
Lazaro Pereira <spmlaz@gmail.com>
2017-06-19 21:16:49 -0400

    Updated to show tunnel status and system-ip

--- Viptela.py
+++ Viptela.py
@@ -4,17 +4,43 @@ import pprint
 
 
 session = requests.session()
-payload = {"j_username":"admin", "j_password": "admin", 'submit': 'Log In'}
-r = session.post(url='https://192.168.250.230/j_security_check', data=payload, verify=False)
+payload = {"j_username":"verizon", "j_password": "Verizon123", 'submit': 'Log In'}
+r = session.post(url='https://127.0.0.1:11443/j_security_check', data=payload, verify=False)
 
 
-data = session.get(url='https://192.168.250.230/dataservice/system/device/vedges/', verify=False)
-serial = '11OD2143152882'
-print(json.loads(data.text)['data'])
+data = session.get(url='https://127.0.0.1:11443/dataservice/system/device/vedges/', verify=False)
+serial = '11OD2143153190'
 for i in json.loads(data.text)['data']:
     #pprint.pprint(json.loads(data.text)['data'][i])
     if serial in i.values():
-        responsedict = {'Device': serial,'Status': i['deviceState'], 'Operating Mode': i['configOperationMode']}
+        try:
+            responsedict = {'Device': serial,'Status': i['deviceState'],
+                        'Operating Mode': i['configOperationMode'],
+                        'systemip': i['system-ip']}
+        except KeyError:
+            responsedict = {'Device': serial, 'Status': i['deviceState'],
+                            'Operating Mode': i['configOperationMode'],
+                            'systemip': 'N/A'}
         print(responsedict)
+
+
+if responsedict['systemip'] != 'N/A':
+    colordata = session.get(url='https://' + '127.0.0.1:11443' +
+                                '/dataservice/device/control/connections?deviceId=' + '172.21.182.36')
+    countgold = 0
+    countprivate1 = 0
+    for i in json.loads(colordata.text)['data']:
+        print(i['local-color'])
+        if 'gold' == i['local-color']:
+            countgold += 1
+        elif 'private1' == i['local-color']:
+            countprivate1 += 1
+        print(countgold)
+        print(countprivate1)
+else:
+    countgold = 'N/A'
+    countprivate1 = 'N/A'
+
+
     #print("not found")
 #pprint.pprint(json.loads(data.text))
--- app/views.py
+++ app/views.py
@@ -24,15 +24,60 @@ def results():
     for i in json.loads(url.text)['data']:
         # pprint.pprint(json.loads(data.text)['data'][i])
         if serial in i.values():
-            responsedict = {'Device': serial,
+            try:
+                responsedict = {'Device': serial,
                             'Status': i['configStatusMessage'],
                             'statusdetail': i['configStatusMessageDetails'],
-                            'operatingmode': i['configOperationMode']}
-        try:
-            responsedict
-        except NameError:
-            responsedict = {'Device': 'Not Found', 'Status': 'Not Found', 'operatingmode': 'Not Found'}
+                            'operatingmode': i['configOperationMode'],
+                            'systemip': i['system-ip']}
+            except KeyError:
+                responsedict = {'Device': serial,
+                            'Status': i['configStatusMessage'],
+                            'statusdetail': i['configStatusMessageDetails'],
+                            'operatingmode': i['configOperationMode'],
+                                'systemip': 'N/A'}
+
+    try:
+        responsedict
+
+    except NameError:
+        responsedict = {'Device': 'Not Found', 'Status': 'Not Found', 'operatingmode': 'Not Found'}
+
+        return render_template('show-results.html',
+                                   data=responsedict, gold='N/A', private1='N/A')
+    countgold = 0
+    countprivate1 = 0
+    if responsedict['systemip'] != 'N/A':
+        colordata = session.get(url='https://' + credentials.server +
+                                    '/dataservice/device/control/connections?deviceId=' + responsedict['systemip'])
+        print(colordata.text)
+
+        for i in json.loads(colordata.text)['data']:
+            print(i['local-color'])
+            if 'gold' == i['local-color']:
+                countgold += 1
+            elif 'private1' == i['local-color']:
+                countprivate1 += 1
+
+    if countgold == 0:
+        goldstatus = 'DOWN'
+
+    if countgold > 0:
+        goldstatus = 'UP'
+
+    if countprivate1 == 0:
+        private1status = 'DOWN'
+
+    if countprivate1 > 0:
+        private1status = 'UP'
+
+    if responsedict['systemip'] == 'N/A':
+        private1status = 'N/A'
+        goldstatus='N/A'
+
 
 
     return render_template('show-results.html',
-                           data=responsedict)
+                           data=responsedict,gold=goldstatus,private1=private1status)
+
+
